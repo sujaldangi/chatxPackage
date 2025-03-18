094a86e71d26fda39d9ce60867400cc6bd06d23a
Shiwani Gupta <shiwani.gupta@infostride.com>
2025-03-18 10:04:17 +0530

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sujal/chatx',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '67abe7e11651cb7b5812c57a57973767530ca129',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sujal/chatx' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '67abe7e11651cb7b5812c57a57973767530ca129',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
